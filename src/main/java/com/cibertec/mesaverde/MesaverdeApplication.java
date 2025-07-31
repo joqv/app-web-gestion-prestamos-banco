10be386b887287da4d42ccaeb7056e3b4f22ab7c
joqv <joseqvme@gmail.com>
2025-07-30 21:39:44 -0500

    proyecto inicializado

---
+++ src/main/java/com/cibertec/mesaverde/MesaverdeApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.mesaverde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesaverdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MesaverdeApplication.class, args);
+	}
+
+}

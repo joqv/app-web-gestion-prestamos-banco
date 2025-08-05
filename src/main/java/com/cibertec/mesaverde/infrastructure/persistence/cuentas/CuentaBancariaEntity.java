cada7c44cf388845e08330cf9622c388bdf1b845
Aldo Eduardo Diaz Gayoso <I202405744@cibertec.edu.pe>
2025-08-04 21:37:08 -0500

    cambiosbancarios

--- src/main/java/com/cibertec/mesaverde/infrastructure/persistence/cuentas/CuentaBancariaEntity.java
+++ src/main/java/com/cibertec/mesaverde/infrastructure/persistence/cuentas/CuentaBancariaEntity.java
@@ -1,4 +1,64 @@
 package com.cibertec.mesaverde.infrastructure.persistence.cuentas;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "cuentas_bancarias")
+@Getter
+@Setter
+@ToString
+
 public class CuentaBancariaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_cuenta")
+    private Integer idCuenta;
+
+
+    @ManyToOne
+    @JoinColumn(name = "id_cliente", nullable = false)
+    private Cliente cliente;
+
+    @ManyToOne
+    @JoinColumn(name = "id_tipo_cuenta", nullable = false)
+    private TipoCuenta tipoCuenta;
+
+    @Column(name = "numero_cuenta", nullable = false, length = 50)
+    private String numeroCuenta;
+
+    @Column(name = "numero_cci_interbancario", length = 50)
+    private String numeroCciInterbancario;
+
+    @Column(name = "saldo", nullable = false, precision = 18, scale = 4)
+    private BigDecimal saldo;
+
+    @ManyToOne
+    @JoinColumn(name = "id_moneda", nullable = false)
+    private Moneda moneda;
+
+    @Column(name = "fecha_apertura")
+    private LocalDateTime fechaApertura;
+
+    @Column(name = "estado_cuenta", length = 20)
+    private String estadoCuenta;
+
+    @Column(name = "fecha_creacion")
+    private LocalDateTime fechaCreacion;
+
+    @Column(name = "fecha_actualizacion")
+    private LocalDateTime fechaActualizacion;
+
+    @Column(name = "usuario_creacion")
+    private Integer usuarioCreacion;
+
+    @Column(name = "usuario_actualizacion")
+    private Integer usuarioActualizacion;
+}
+
 }
--- src/main/java/com/cibertec/mesaverde/infrastructure/persistence/cuentas/TipoCuentaEntity.java
+++ src/main/java/com/cibertec/mesaverde/infrastructure/persistence/cuentas/TipoCuentaEntity.java
@@ -1,4 +1,41 @@
 package com.cibertec.mesaverde.infrastructure.persistence.cuentas;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table (name = "tipo_cuenta")
+@Getter
+@Setter
+@ToString
+
 public class TipoCuentaEntity {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column(name = "id_tipo_cuenta")
+    private Integer idTipoCuenta;
+
+    @Column(name = "nombre", nullable = false, length = 50)
+    private String nombre;
+
+    @Column(name = "descripcion", length = 255)
+    private String descripcion;
+
+    @Column(name = "fecha_creacion")
+    private LocalDateTime fechaCreacion;
+
+    @Column(name = "fecha_actualizacion")
+    private LocalDateTime fechaActualizacion;
+
+    @Column(name = "usuario_creacion")
+    private Integer usuarioCreacion;
+
+    @Column(name = "usuario_actualizacion")
+    private Integer usuarioActualizacion;
 }
+

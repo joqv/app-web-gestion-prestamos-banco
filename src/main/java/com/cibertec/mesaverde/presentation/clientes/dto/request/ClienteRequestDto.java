eb6e4d9f28eaf6754fd616371128e59bf697c269
joqv <joseqvme@gmail.com>
2025-08-19 12:07:49 -0500

    cambiado secret-key generado con el test del profesor

--- src/main/java/com/cibertec/mesaverde/presentation/clientes/dto/request/ClienteRequestDto.java
+++ src/main/java/com/cibertec/mesaverde/presentation/clientes/dto/request/ClienteRequestDto.java
@@ -1,5 +1,6 @@
 package com.cibertec.mesaverde.presentation.clientes.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,11 +10,17 @@ import java.time.LocalDate;
 @Builder
 public class ClienteRequestDto {
 
+    @NotBlank(message = "El nombre del cliente es obligatorio.")
     private String nombre;
+    @NotBlank(message = "El nombre del apellido es obligatorio.")
     private String apellido;
+    @NotBlank(message = "El tipo de documento es obligatorio.")
     private String tipoDocumento;
+    @NotBlank(message = "El número de documento es obligatorio.")
     private String numeroDocumento;
+    @NotBlank(message = "La fecha de nacimiento es obligatoria.")
     private LocalDate fechaNacimiento;
+    @NotBlank(message = "La dirección es obligatoria.")
     private String direccion;
     private String telefono;
     private String email;
